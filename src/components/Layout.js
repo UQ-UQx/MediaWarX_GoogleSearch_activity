0a761bfaf127b4d4b6428ca5ba49ab8b39cc14a9
Ankith Konda <ankith.konda@gmail.com>
2017-04-07 21:27:36 +1000

    Add pagination boilerplate

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,16 @@
 import React from "react"
 
-import GoogleSearchUploadForm from "./GoogleSearchUploadForm"
+import GoogleSearchUploadFormPage from "./GoogleSearchUploadFormPage"
+import GoogleSearchMapPage from "./GoogleSearchMapPage"
+
+import each from "lodash/each"
+
 
 export default class Layout extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            selected_page:"upload", // options: upload | map
-
+            selected_page:"form_page", // options: upload | map
 
             location_name:'',
             location_lat:null,
@@ -36,6 +39,11 @@ export default class Layout extends React.Component {
 
         this.handleFormSubmit = this.handleFormSubmit.bind(this);
         this.handleUploadFormItemUpdate = this.handleUploadFormItemUpdate.bind(this);
+        this.checkFormRequirementsMet = this.checkFormRequirementsMet.bind(this);
+
+        this.handlePageButtonClick = this.handlePageButtonClick.bind(this);
+        // this.handleUploadFormPageButtonClick = this.handleUploadFormPageButtonClick.bind(this);
+        // this.handleMapPageButtonClick = this.handleMapPageButtonClick.bind(this);
     }
     componentWillMount(){
         console.log("Layout component will mount");
@@ -52,54 +60,112 @@ export default class Layout extends React.Component {
         this.setState(item);
 
     }
-
     handleFormSubmit(){
-
-
         console.log("form submit clicked");
     }
+    checkFormRequirementsMet(){
+        var required = [
+            "location_name",
+            "location_lat",
+            "location_lng",
+            "age",
+            "gender",
+            "nationality",
+            "education",
+            "tags",
+            "location_static_map",
+            "image_file"
+        ]
+
+        var metRequirements = true;
+        console.log("--------------");
+
+        each(required, (val, key)=>{
+            if(!this.state[val]){
+                console.log(val);
+                metRequirements = false;
+            }
+
+            if(val == "tags"){
+                if(this.state[val].length == 0){
+                    console.log(val);
+                    metRequirements = false;
+                }
+            }
+        })
 
 
+        return metRequirements
+    }
+    handlePageButtonClick(page){
 
+        console.log("Page button clicked", page);
 
+        this.setState({"selected_page":page})
+
+    }
+    renderGoogleSearchUploadFormPage(){
+        return (<GoogleSearchUploadFormPage
+
+            location_name={this.state.location_name}
+            location_lat={this.state.location_lat}
+            location_lng={this.state.location_lng}
+            location_suggestion_fetching={this.state.location_suggestion_fetching}
+            location_suggestion={this.state.location_suggestion}
+            location_error={this.state.location_error}
+
+            age={this.state.age}
+            gender={this.state.gender}
+            nationality={this.state.nationality}
+            education={this.state.education}
+
+            image_file={this.state.image_file}
+
+            tag={this.state.tag}
+            tags={this.state.tags}
+
+            suggested_tags={this.state.suggested_tags}
+            suggested_tags_fetching={this.state.suggested_tags_fetching}
+            suggested_tags_error={this.state.suggested_tags_error}
+
+            location_static_map={this.state.location_static_map}
+
+            checkFormRequirementsMet={this.checkFormRequirementsMet}
+            handleUploadFormItemUpdate={this.handleUploadFormItemUpdate}
+            handleFormSubmit={this.handleFormSubmit}
+
+        />)
+    }
+    renderGoogleSearchMapPage(){
+        return(<GoogleSearchMapPage
+
+            
+
+        />)
+    }
 
     render(){
+
+        var page = this.renderGoogleSearchUploadFormPage();
+
+        switch (this.state.selected_page) {
+            case "form_page":
+                page = this.renderGoogleSearchUploadFormPage()
+                break;
+            case "map_page":
+                page = this.renderGoogleSearchMapPage()
+                break;
+            default:
+        }
 
 
         return (
         <div className="layout-component">
 
-            --- page buttons go here for ---
+            <button className="btn btn-default btn-med page-button" onClick={()=>this.handlePageButtonClick("form_page")}>Upload Form</button>
+            <button className="btn btn-default btn-med page-button" onClick={()=>this.handlePageButtonClick("map_page")}>Map Page</button>
 
-            <GoogleSearchUploadForm
-
-                location_name={this.state.location_name}
-                location_lat={this.state.location_lat}
-                location_lng={this.state.location_lng}
-                location_suggestion_fetching={this.state.location_suggestion_fetching}
-                location_suggestion={this.state.location_suggestion}
-                location_error={this.state.location_error}
-
-                age={this.state.age}
-                gender={this.state.gender}
-                nationality={this.state.nationality}
-                education={this.state.education}
-
-                image_file={this.state.image_file}
-
-                tag={this.state.tag}
-                tags={this.state.tags}
-
-                suggested_tags={this.state.suggested_tags}
-                suggested_tags_fetching={this.state.suggested_tags_fetching}
-                suggested_tags_error={this.state.suggested_tags_error}
-
-                location_static_map={this.state.location_static_map}
-
-                handleUploadFormItemUpdate={this.handleUploadFormItemUpdate}
-
-            />
-
+            {page}
 
         </div>);
     }
